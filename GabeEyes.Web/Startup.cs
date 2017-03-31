723e561000691993ba16337ff9affb083008eba5
Devin Turner <turnerd18@gmail.com>
2017-03-31 09:16:30 -0500

    include the wwwroot app build

--- GabeEyes.Web/Startup.cs
+++ GabeEyes.Web/Startup.cs
@@ -15,7 +15,7 @@ namespace GabeEyes.Web
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
             Configuration = builder.Build();
